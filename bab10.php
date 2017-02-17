1741405dea058af40b09db50cd6061be8e059d62
kh0o0li5 <otjt37@gmail.com>
2017-02-17 20:57:52 +0700

    megambil id terakhir

---
+++ bab10.php
@@ -0,0 +1,26 @@
+<?php
+
+  //bab10 memgambil ID terakhir
+  $host="localhost";
+  $dbname="coba";
+
+  $user="root";
+  $password="";
+
+    try {
+      $pdo=new PDO ("mysql:dbname=$dbname;host=$host", $user,$password);
+      $stmt=$pdo->prepare("INSERT INTO user (username) VALUES (?)");
+      $stmt->bindValue(1, "slamet");
+
+      if($stmt->execute()){
+        $id=$pdo->lastInsertId();
+        echo "id terakhir :" .$id."<br";
+      }
+
+    } catch (PDOException $e) {
+      
+    }
+
+    
+
+?>
